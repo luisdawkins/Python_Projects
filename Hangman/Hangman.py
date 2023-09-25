69f2012987bb49dfb68093605b557918cc1d385f
luisdawkins <107372925+luisdawkins@users.noreply.github.com>
2023-09-25 09:14:21 -0400

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,1 +1,85 @@
+"""
+Author: Luis Dawkins
+Date: 9/23/23
+This program runs the famous hangman game for a user.
+"""
 
+import random
+from words import word_list
+
+def Get_Words():
+
+#Display 
+def Display_Hangman(Tries):
+    Stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return Stages[Tries]
