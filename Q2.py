73dd1a43299498b280a0b92739f488469a5d7a5c
luisdawkins <107372925+luisdawkins@users.noreply.github.com>
2023-10-15 10:48:34 -0400

    Delete Q2.py

--- Q2.py
+++
@@ -1,6 +0,0 @@
-"""
-
-Write a program that reads a character, then reads in a list of words. The output of the program is every
-word in the list that contains the character at least once. Assume at least one word in the list will
-contain the given character.
-"""
