3ebaf5a55ffeea0844ae25209e41b43fa212caee
luisdawkins <107372925+luisdawkins@users.noreply.github.com>
2023-10-15 21:03:01 -0400

    Add files via upload

---
+++ Assignment4_LuisDawkins/Q3.py
@@ -0,0 +1,12 @@
+"""
+Author: Luis Dawkins
+Date: 10/15/23
+This program analyzes a list of words to count frequency of each word, then outputs the results.
+
+Write a program that reads a list of words. Then, the program outputs those words
+and their frequencies(case insensitive).
+
+1. Input
+    Initialize text
+
+"""
