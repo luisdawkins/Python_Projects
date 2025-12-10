a358651c823ec2c89a9f5bdf47dfd9b85b8bede6
luisdawkins <107372925+luisdawkins@users.noreply.github.com>
2025-12-10 13:35:41 -0500

    Update Class_Instance.py
    added a more advance feature

--- Misc/Class_Instance.py
+++ Misc/Class_Instance.py
@@ -15,3 +15,18 @@ bobs_burgers.rating = 4.7
 bobs_burgers.delivery = False
 
 print(vars(bobs_burgers))
+
+# Write code below 💖
+class City:
+    #__init__ and the self argument are used to initialize classes and make assigning instance variables more efficient
+    def __init__(self, name, country, population, landmarks):
+        self.name = name
+        self.country = country
+        self.population = population
+        self.landmarks = landmarks
+
+Home = City("Troy", "United States", 70000, ["Somerset Mall", "Big Beaver"])
+Travel = City("Chicago", "United States", 3000000, ["Al Capone's House", "The Bean", "Pizza Parlor"])
+
+print(vars(Home))
+print(vars(Travel))
