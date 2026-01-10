45ad48d12e68ab9608ad30baaae1853382000265
luisdawkins <107372925+luisdawkins@users.noreply.github.com>
2026-01-10 15:51:22 -0500

    Update advanced_file_operations.py

--- Misc/advanced_file_operations.py
+++ Misc/advanced_file_operations.py
@@ -11,17 +11,19 @@ it will create the csv file for future use.
     a. Create try/except block
     b. Configure file handling method within try block
     c. Iterate through every row within csv file
-    d. Print each row of csv file
-    e. Configure write method within except block
-    f. Write key information to csv file
-3. output
+    d. Configure write method within except block
+    e. Write key information to csv file
+3. Output
     a. Print each row of information
     b. Print confirmation message upon successful or failed execution
 
 """
 import csv
 
+### Input
+#Define file path
 file_path = "packing_list.csv"
+#Define Data
 data = [
     ['Item', 'Quantity'],
     ['Blender', 2],
@@ -29,19 +31,26 @@ data = [
     ['Shoes', 2]
 ]
 
+### Processing
+#Create try/except block
 try:
-    #the encoding parameter is to prevent configuration issues
+    #Configure file handling method within try block
+    #the encoding arguement is to prevent configuration issues
     with open(file_path, 'r', encoding='utf-8') as file:
         #Configure file object
         csv_reader = csv.reader(file)
-        #iterate through every row of csv file
+        #Iterate through every row within csv file
         for row in csv_reader:
+            #Print each row of information
             print(row)
 
     print("\nData has been successfully written.")
 except FileNotFoundError:
+    #Print confirmation message upon successful or failed execution
     print("Packing list file not found. Creating a new one.")
+    #Configure write method within except block
     with open(file_path, 'w', newline="") as file:
         csv_writer = csv.writer(file)
         for row in data:
+            #Write key information to csv file
             csv_writer.writerow(row)
